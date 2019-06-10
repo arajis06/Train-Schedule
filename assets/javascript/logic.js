1d435c0043b80409a5a7d053112cb498faf3d44b
ALEXIS RAJIS <arajis06@gmail.com>
2019-06-10 17:58:59 -0400

    finished html

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDSepEZJjZzCLvts1QGzaVPNUExE_SkkB4",
+    authDomain: "train-schedule-7685e.firebaseapp.com",
+    databaseURL: "https://train-schedule-7685e.firebaseio.com",
+    projectId: "train-schedule-7685e",
+    storageBucket: "train-schedule-7685e.appspot.com",
+    messagingSenderId: "337294394288",
+    appId: "1:337294394288:web:d644743fc03b9f0c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
